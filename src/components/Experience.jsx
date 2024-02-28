e4fc145a4263ed85440a036d37ac56d16def8064
Ranga Dananjaya <rangamaithreepala@gmail.com>
2024-02-28 19:33:12 +0530

    v2.1.3

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -60,10 +60,10 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+        What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
+          My journey.
         </h2>
       </motion.div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-16 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Ranga &nbsp;
-            <span className='sm:block hidden'> | Portfolio</span>
+            <span className='sm:block hidden'> Dananjaya</span>
           </p>
         </Link>
 
