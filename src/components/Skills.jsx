869b4c774d231710d7a6581ddaaa7bae32cabfa3
Ranga Dananjaya <rangamaithreepala@gmail.com>
2024-05-15 08:11:22 +0530

    Add React Slick carousel and update Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,8 +6,22 @@ import { textVariant } from "../utils/motion";
 import { styles } from "../styles";
 import TrackVisibility from "react-on-screen";
 import rocket from "../assets/rocket.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { useRef } from "react";
 
 const Skills = () => {
+  const sliderRef = useRef(null);
+  const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+};
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -19,20 +33,20 @@ const Skills = () => {
       <br />
       <br />
       <div>
-        <div className="flex flex-row flex-wrap justify-center gap-10">
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styless.skill}>
-                <div
-                  className={`relative overflow-auto ${styless.skillImageContainer}`}
-                >
-                  <img src={skill.imageSrc} alt={skill.title} />
-                </div>
-                <p>{skill.title}</p>
-              </div>
-            );
-          })}
-        </div>
+      <div>
+    <Slider ref={sliderRef} {...settings}>
+      {skills.map((skill, id) => {
+        return (
+          <div key={id} className={styless.skill}>
+            <div className={`relative overflow-auto ${styless.skillImageContainer}`}>
+              <img src={skill.imageSrc} alt={skill.title} />
+            </div>
+            <p>{skill.title}</p>
+          </div>
+        );
+      })}
+    </Slider>
+  </div>
         <div className="banner p-1 pt-14 flex items-center justify-center">
           <TrackVisibility>
             {({ isVisible }) => (
