d4c7f9b48198beec8c3f33c6bd8439c852492452
Ranga Dananjaya <rangamaithreepala@gmail.com>
2024-05-16 07:30:13 +0530

    change banner page icon

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import propic from "../assets/propic.png";
 import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
 import { fadeIn } from "../utils/motion";
-import resume from "../assets/Ranga_Intern_cv.pdf"
+import resume from "../assets/Ranga_Intern_cv.pdf";
 
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -101,17 +101,18 @@ const Banner = () => {
 
       <div className="absolute xs:bottom-10 lg:bottom-36 md:bottom-36 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[40px] h-[70px] rounded-full border-4 border-secondary flex justify-center items-center p-2">
             <motion.div
               animate={{
-                y: [0, 24, 0],
+                scale: [1, 1.5, 1],
+                opacity: [1, 0.5, 1],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-10 h-10 rounded-full bg-secondary"
             />
           </div>
         </a>
